15d544381ddc9a6205b68fc0d34d2c5781d05ffb
luizfiliperm <luizfilipemr17@gmail.com>
2023-11-01 11:24:07 -0300

    Implementing random numbers generations to populate the array

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,16 +13,28 @@ public class Main {
 
         int size = sc.nextInt();
 
+        System.out.println("Do you want to generate random array? (Y/N)");
+        System.out.print("Your choice: ");
+        String choice = sc.next().toUpperCase();
+
         divider();
 
         int[] array = new int[size];
 
-        System.out.println("Please enter the elements of array: ");
-        for(int i = 0; i < size; i++) {
-            System.out.print("Element " + (i + 1) + ": ");
-            array[i] = sc.nextInt();
+        if(choice.equals("Y")){
+            for(int i = 0; i < size; i++) {
+                array[i] = (int) (Math.random() * 100);
+            }
+        }else{
+            System.out.println("Please enter the elements of array: ");
+            for(int i = 0; i < size; i++) {
+                System.out.print("Element " + (i + 1) + ": ");
+                array[i] = sc.nextInt();
+            }
+            divider();
         }
-
+        System.out.println("Your array : ");
+        printArray(array);
         divider();
 
         System.out.println("Choose the algorithm you want to use: ");
@@ -32,22 +44,18 @@ public class Main {
         System.out.println("4. Merge Sort");
         System.out.println("5. Quick Sort");
 
-        int choice;
+        int option;
         do{
-
-        System.out.print("Your choice: ");
-        choice = sc.nextInt();
+        System.out.print("Your option: ");
+        option = sc.nextInt();
         divider();
-        if(choice < 1 || choice > 5)
-            System.out.println("Invalid choice! Please choose again!");
-        }while(choice < 1 || choice > 5);
+
+        if(option < 1 || option > 5)
+            System.out.println("Invalid option! Please choose again!");
+        }while(option < 1 || option > 5);
 
 
-        System.out.println("Your array before sorting: ");
-        printArray(array);
-
-
-        String algorithm = switch (choice) {
+        String algorithm = switch (option) {
             case 1 -> {
                 BubbleSort.sort(array);
                 yield "Bubble Sort";
@@ -75,7 +83,6 @@ public class Main {
         System.out.printf("\nYour array after " + algorithm +" algorithm: \n");
         printArray(array);
         divider();
-
         sc.close();
     }
 
